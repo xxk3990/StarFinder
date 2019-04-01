0dafc2bf3672c45dc517df284eda37462b8a0397
Emily Horton (RIT Student) <eh8582@ad.rit.edu>
2019-04-01 18:08:20 -0400

    creates/sends url

--- project2/scripts.js
+++ project2/scripts.js
@@ -1,22 +1,50 @@
 "use strict"
 
+//Event
+window.onload = (e) => {document.querySelector("button").onclick = search};
+
 function search() {
     const STAR_URL = "http://swapi.co/api/";
     let url = STAR_URL;
     let searchTerm = document.querySelector("#search");
     let displayTerm = "";
     searchTerm = displayTerm;
-    searchTerm.searchTerm.trim();
+    searchTerm = searchTerm.trim();
     searchTerm = encodeURIComponent(searchTerm);
     if(searchTerm.length > 1) {
         return;
     }
-    url += "&q=" + searchTerm;
+
     let filter = document.querySelector("#filter");
-    url += "films/" + filter + "/";
+    url += "films/" + filter.options[filter.selectedIndex].value + "/";
+
+    //Logs URL for debug, request data
+    console.log(url);
+    getData(url);
 
 }
 
+//Gets data from generated URL
+function getData(url){
+    let xhr = new XMLHttpRequest();
+
+    xhr.onerror = dataError;
+    xhr.onload = dataLoaded;
+
+    xhr.open("GET", url);
+    xhr.send();
+}
+
+//Callback functions
+function dataLoaded(e){
+    let xhr = e.target;
+
+    console.log(xhr.responseText);
+}
+
+function dataError(e){
+    console.log("An error occured when requesting data!");
+}
 
 
 $("#flip").flip({
