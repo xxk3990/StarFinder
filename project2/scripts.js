805de2ee14e441baf736e99e85530e6b88760516
Emily Horton (RIT Student) <eh8582@ad.rit.edu>
2019-04-01 19:58:38 -0400

    Retrieves info of 3 random characters by film

--- project2/scripts.js
+++ project2/scripts.js
@@ -1,12 +1,14 @@
 "use strict"
 
 //Event
-//why are changes not registering ahhh
-window.onload = (e) => {document.querySelector("button").onclick = search};
+window.onload = (e) => {document.querySelector("button").onclick = searchByFilm};
 
-function search() {
+//Proxy URL is used due to SWAPI not having CORS enabled.
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
+//Searches for a film, then gets random characters from the film.
+function searchByFilm() {
     const STAR_URL = "http://swapi.co/api/";
-    const proxyurl = "https://cors-anywhere.herokuapp.com/"; //Proxy needed for access via cors
     let url = STAR_URL;
     let searchTerm = document.querySelector("#search");
     let displayTerm = "";
@@ -20,35 +22,99 @@ function search() {
     let filter = document.querySelector("#filter");
     url += "films/" + filter.options[filter.selectedIndex].value + "/";
 
-    //Logs URL for debug, request data
-    console.log(url);
-    getData(proxyurl + url);
-
+    //Random characters will need a different function than characters by film, and searching.
+    getRandomCharacters(proxyurl + url);
 }
 
-//Gets data from generated URL
-function getData(url){
+//
+//Data Requests
+//
+
+function getRandomCharacters(url){
     let xhr = new XMLHttpRequest();
 
     xhr.onerror = dataError;
-    xhr.onload = dataLoaded;
+    xhr.onload = filmDataLoaded;
 
     xhr.open("GET", url);
     xhr.send();
 }
 
+//Get specific character info from a supplied URL
+function getCharacterData(url){
+    let xhr = new XMLHttpRequest();
+
+    xhr.onerror = dataError;
+    xhr.onload = characterDataLoaded;
+
+    xhr.open("GET", proxyurl + url);
+    xhr.send();
+}
+
+//
+//End Data Requests
+//
+
+
+//Gets three random characters from a film.
+function getCharactersFromFilm(data){
+
+    //Parse response data
+    let object = JSON.parse(data); //Usabale JS object
+    let possibleCharacters = object.characters; //All of the characters
+
+    //Randomly selects three characters
+    let selectedCharacters =[];
+    for(let i = 0; i< 3; i++)
+    {
+        selectedCharacters.push(possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)]);
+
+        //Now access character data
+        getCharacterData(selectedCharacters[i]);
+    }
+}
+
+//Called to parse in character data
+function parseCharacterData(data)
+{
+    let object = JSON.parse(data);
+
+    //All info about the characters is grabbed here.
+    //For now these are debug logs, but eventually will generate HTML elements.
+    console.log("Name: " + object.name);
+    console.log("Gender: " + object.gender);
+    console.log("Birth Year: " + object.birth_year);
+    console.log("Hair Color: " + object.hair_color);
+    console.log("Eye Color: " + object.eye_color);
+}
+
+//
 //Callback functions
-function dataLoaded(e){
+//
+function filmDataLoaded(e){
     let xhr = e.target;
 
-    console.log(xhr.responseText);
+    //Loads characters on successful request
+    getCharactersFromFilm(xhr.responseText);
+}
+
+function characterDataLoaded(e){
+    let xhr = e.target;
+
+    //Parse character data on successful request
+    parseCharacterData(xhr.responseText)
 }
 
 function dataError(e){
     console.log("An error occured when requesting data!");
 }
 
+//
+//End callback functions
+//
 
+
+//jQuery flipping behaviour
 $(".flip").flip({
     trigger: 'click',
     reverse: false,
