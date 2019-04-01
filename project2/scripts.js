bd12d571fce5e0c05d8a4cf4f4ff41d553a1b16b
Xander Kaylan <xxk3990@rit.edu>
2019-04-01 17:21:16 -0400

    added stuff

--- project2/scripts.js
+++ project2/scripts.js
@@ -1,2 +1,26 @@
 "use strict"
 
+function search() {
+    const STAR_URL = "http://swapi.co/api/";
+    let url = STAR_URL;
+    let searchTerm = document.querySelector("#search");
+    let displayTerm = "";
+    searchTerm = displayTerm;
+    searchTerm.searchTerm.trim();
+    searchTerm = encodeURIComponent(searchTerm);
+    if(searchTerm.length > 1) {
+        return;
+    }
+    url += "&q=" + searchTerm;
+    let filter = document.querySelector("#filter");
+    url += "films/" + filter + "/";
+
+}
+
+
+
+$("#flip").flip({
+    trigger: 'click',
+    reverse: false,
+    axis: 'y'
+});
