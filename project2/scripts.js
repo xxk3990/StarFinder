47e8192acbd59c38789baf0cc899c60dfb4cd563
Emily Horton <emilyhorton1994@gmail.com>
2019-04-02 16:53:03 -0400

    Reorganized code

--- project2/scripts.js
+++ project2/scripts.js
@@ -1,7 +1,7 @@
 "use strict"
 
 //Event
-window.onload = (e) => {document.querySelector("button").onclick = searchByFilm};
+window.onload = (e) => {document.querySelector("button").onclick = getAllCharacters};
 
 //Proxy URL is used due to SWAPI not having CORS enabled.
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
@@ -9,10 +9,11 @@ const proxyurl = "https://cors-anywhere.herokuapp.com/";
 //Google API key
 const googleKey = "AIzaSyD_zvFPiBpHdqe-_uWzyNxgaw0PU9YMi-E";
 
-//Searches for a film, then gets random characters from the film.
-function searchByFilm() {
-    const STAR_URL = "http://swapi.co/api/";
+//Get all characters
+function getAllCharacters() {
+    const STAR_URL = "http://swapi.co/api/people/";
     let url = STAR_URL;
+    /*
     let searchTerm = document.querySelector("#search");
     let displayTerm = "";
     searchTerm = displayTerm;
@@ -24,16 +25,14 @@ function searchByFilm() {
 
     let filter = document.querySelector("#filter");
     url += "films/" + filter.options[filter.selectedIndex].value + "/";
+    */
 
-    //Random characters will need a different function than characters by film, and searching.
-    getRandomCharacters(proxyurl + url);
+    //Now that the url is created, request the character data
+    requestCharacterData(proxyurl + url);
 }
 
-//
-//Data Requests
-//
 
-function getRandomCharacters(url){
+function requestCharacterData(url){
     let xhr = new XMLHttpRequest();
 
     xhr.onerror = dataError;
@@ -41,6 +40,33 @@ function getRandomCharacters(url){
 
     xhr.open("GET", url);
     xhr.send();
+}
+
+function filmDataLoaded(e){
+    let xhr = e.target;
+
+    //Loads characters on successful request
+    getCharactersFromFilm(xhr.responseText);
+}
+
+//Gets three random characters from a film.
+function getCharactersFromFilm(data){
+
+    //Parse response data
+    let object = JSON.parse(data); //Usabale JS object
+    let possibleCharacters = object.results; //All of the characters
+
+    /*Randomly selects three characters
+    let selectedCharacters =[];
+    for(let i = 0; i< 3; i++)
+    {
+        selectedCharacters.push(possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)]);
+
+        //Now access character data
+        getCharacterData(selectedCharacters[i]);
+    }
+    */
+   console.log(possibleCharacters);
 }
 
 //Get specific character info from a supplied URL
@@ -54,43 +80,11 @@ function getCharacterData(url){
     xhr.send();
 }
 
-//Get data from google
-function getGoogleImageData(url)
-{
-    let xhr = new XMLHttpRequest();
+function characterDataLoaded(e){
+    let xhr = e.target;
 
-    xhr.onerror = dataError;
-    xhr.onload = googleImageDataLoaded;
-
-    xhr.open("Get", url);
-    xhr.send();
-}
-
-//this might be useful
-//https://www.googleapis.com/customsearch/v1?key=YOUR-KEY&cx=017576662512468239146:omuauf_lfve&q=tomato&callback=hndlr
-
-
-//
-//End Data Requests
-//
-
-
-//Gets three random characters from a film.
-function getCharactersFromFilm(data){
-
-    //Parse response data
-    let object = JSON.parse(data); //Usabale JS object
-    let possibleCharacters = object.characters; //All of the characters
-
-    //Randomly selects three characters
-    let selectedCharacters =[];
-    for(let i = 0; i< 3; i++)
-    {
-        selectedCharacters.push(possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)]);
-
-        //Now access character data
-        getCharacterData(selectedCharacters[i]);
-    }
+    //Parse character data on successful request
+    parseCharacterData(xhr.responseText)
 }
 
 //Called to parse in character data
@@ -107,31 +101,10 @@ function parseCharacterData(data)
     console.log("Eye Color: " + object.eye_color);
 }
 
-//
-//Callback functions
-//
-function filmDataLoaded(e){
-    let xhr = e.target;
-
-    //Loads characters on successful request
-    getCharactersFromFilm(xhr.responseText);
-}
-
-function characterDataLoaded(e){
-    let xhr = e.target;
-
-    //Parse character data on successful request
-    parseCharacterData(xhr.responseText)
-}
-
+//Function will report an error fetching data
 function dataError(e){
     console.log("An error occured when requesting data!");
 }
-
-//
-//End callback functions
-//
-
 
 //jQuery flipping behaviour
 $(".flip").flip({
@@ -139,3 +112,20 @@ $(".flip").flip({
     reverse: false,
     axis: 'y'
 });
+
+/* //Eventually used for image lookup
+//Get data from google
+function getGoogleImageData(url)
+{
+    let xhr = new XMLHttpRequest();
+
+    xhr.onerror = dataError;
+    xhr.onload = googleImageDataLoaded;
+
+    xhr.open("Get", url);
+    xhr.send();
+}
+
+this might be useful
+https://www.googleapis.com/customsearch/v1?key=YOUR-KEY&cx=017576662512468239146:omuauf_lfve&q=tomato&callback=hndlr
+*/
