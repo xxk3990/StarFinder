79c6fc56972d560a10074ded75d1527a4ec98bc4
Xander Kaylan <xxk3990@rit.edu>
2019-04-06 14:00:15 -0400

    added search stuff to js

--- project2/scripts.js
+++ project2/scripts.js
@@ -22,31 +22,37 @@ function getAllCharacters() {
 
     const STAR_URL = "http://swapi.co/api/";
     let url = STAR_URL;
-    
-    let searchTerm = document.querySelector("#search");
-    let displayTerm = "";
-    searchTerm = displayTerm;
-    searchTerm = searchTerm.trim();
-    searchTerm = encodeURIComponent(searchTerm);
-    if(searchTerm.length > 1) {
-        return;
-    }
 
+    let searchTerm = document.querySelector("#search").value;
+    // let displayTerm = searchTerm.innerHTML;
+    console.log(searchTerm);
+
+    if (searchTerm != "") {
+        console.log(searchTerm);
+        searchTerm = searchTerm.trim();
+        searchTerm = encodeURIComponent(searchTerm);
+        url += "people/?search=" + searchTerm;
+        requestSearch(proxyurl + url);
+    }
     let filter = document.querySelector("#filter");
     let selectedMovie = filter.options[filter.selectedIndex].value
-    
-    //Pick a random movie if the user didn't pick one
-    while (selectedMovie == 0) 
-    {selectedMovie = Math.floor(Math.random() * 6)}
 
+    //Pick a random movie if the user didn't pick one
+    while (selectedMovie == 0) {
+        selectedMovie = Math.floor(Math.random() * 6)
+    }
+    url = STAR_URL;
     url += "films/" + selectedMovie + "/";
 
     //Now that the url is created, request the character data
     requestCharacterData(proxyurl + url);
+
+
+
 }
 
 //Responsible for making the server request
-function requestCharacterData(url){
+function requestCharacterData(url) {
     status.innerHTML = "Status: Requesting info from SWAPI"
     let xhr = new XMLHttpRequest();
 
@@ -57,25 +63,48 @@ function requestCharacterData(url){
     xhr.send();
 }
 
+function requestSearch(url) {
+    status.innerHTML = "Status: Requesting info from SWAPI"
+    let xhr = new XMLHttpRequest();
+
+    xhr.onerror = dataError;
+    xhr.onload = searchDataLoaded;
+
+    xhr.open("GET", url);
+    xhr.send();
+}
+
 //When data is loaded, this grabs the response
-function filmDataLoaded(e){
+function filmDataLoaded(e) {
     let xhr = e.target;
 
     //Loads characters on successful request
     getCharactersFromFilm(xhr.responseText);
 }
 
+function searchDataLoaded(e) {
+    let xhr = e.target;
+    getSearchResult(xhr.responseText);
+}
+
+function getSearchResult(data) {
+    let object = JSON.parse(data);
+    console.log(data);
+    let result = object.results;
+    console.log(data.results);
+    getSearchInfo(result);
+}
+
 //Gets three random characters from a film.
-function getCharactersFromFilm(data){
+function getCharactersFromFilm(data) {
 
     //Parse response data
     let object = JSON.parse(data); //Usabale JS object
     let possibleCharacters = object.characters; //All of the characters
 
     //Randomly selects three characters
-    let selectedCharacters =[];
-    for(let i = 0; i< 3; i++)
-    {
+    let selectedCharacters = [];
+    for (let i = 0; i < 3; i++) {
         selectedCharacters.push(possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)]);
 
         //Now access character data
@@ -84,7 +113,7 @@ function getCharactersFromFilm(data){
 }
 
 //Get characters from film gave us url's to character info; now we request data from those urls
-function getCharacterData(url){
+function getCharacterData(url) {
     let xhr = new XMLHttpRequest();
     xhr.onerror = dataError;
     xhr.open("GET", proxyurl + url);
@@ -98,24 +127,50 @@ function getCharacterData(url){
     };
 }
 
+function getSearchInfo(url) {
+    let xhr = new XMLHttpRequest();
+    xhr.onerror = dataError;
+    xhr.open("GET", proxyurl + url);
+    console.log(url);
+    xhr.send();
+    xhr.onload = (e) => {
+        let xhr = e.target;
+        parseSearchData(xhr.responseText);
+    }
+
+
+}
+
 //Called to parse in character data
-function parseCharacterData(data)
-{
+function parseCharacterData(data) {
 
     let object = JSON.parse(data);
 
-        if (characterNumber == 3) {characterNumber = 0}
-        else if (characterNumber == 2) {status.innerHTML = "Status: Complete!";}
-        characterNumber++;
-        let backData = document.querySelector(".data" + characterNumber);
-        backData.innerHTML =  "Name: " + object.name + "<br>" +
-          "Gender: " + object.gender + "<br>" + "Birth Year: " + object.birth_year + "<br>" +"Hair Color: " + object.hair_color + "<br>" + 
-          "Eye Color: " + object.eye_color;
+    if (characterNumber == 3) {
+        characterNumber = 0
+    } else if (characterNumber == 2) {
+        status.innerHTML = "Status: Complete!";
+    }
+    characterNumber++;
+    let backData = document.querySelector(".data" + characterNumber);
+    backData.innerHTML = "Name: " + object.name + "<br>" +
+        "Gender: " + object.gender + "<br>" + "Birth Year: " + object.birth_year + "<br>" + "Hair Color: " + object.hair_color + "<br>" +
+        "Eye Color: " + object.eye_color;
+
+
+
+}
+
+function parseSearchData(data) {
+    let searchData = document.querySelector(".searchData");
+    searchData.innerHTML = "Name: " + data.name + "<br>" +
+        "Gender: " + data.gender + "<br>" + "Birth Year: " + data.birth_year + "<br>" + "Hair Color: " + data.hair_color + "<br>" +
+        "Eye Color: " + data.eye_color;
 
 }
 
 //Function will report an error fetching data
-function dataError(e){
+function dataError(e) {
     console.log("An error occured when requesting data!");
 }
 
